cc3420b62bde924deacc7aace2edbff4f95e9bee
gffuma <fumagalli.gf@gmail.com>
2016-09-29 17:52:35 +0200

    Me endpoint

--- cmz_admin_api/api/urls.py
+++ cmz_admin_api/api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib.auth.models import User
 from rest_framework import routers
-from .views import NewsViewSet
+from .views import NewsViewSet, MeView
 from rest_framework_jwt.views import obtain_jwt_token
 
 router = routers.DefaultRouter()
@@ -9,5 +9,6 @@ router.register(r'news', NewsViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^me/', MeView.as_view()),
     url(r'^auth/', obtain_jwt_token)
 ]
--- cmz_admin_api/api/views.py
+++ cmz_admin_api/api/views.py
@@ -1,5 +1,18 @@
-from rest_framework import serializers, viewsets
+from rest_framework import serializers, viewsets, views
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 from .models import News
+
+# Me
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username')
+
+class MeView(views.APIView):
+    def get(self, request, format=None):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
 
 # News
 class NewsSerializer(serializers.ModelSerializer):
